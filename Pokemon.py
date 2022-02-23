15bc590dfdd5e6aed8e735a451d665a5fd561b2b
Joseph2403 <100103220+Joseph2403@users.noreply.github.com>
2022-02-23 22:52:17 +0530

    Update Pokemon.py

--- Pokemon.py
+++ Pokemon.py
@@ -1,8 +1,11 @@
 import pyttsx3
+import openpyxl as xl
 
 engine = pyttsx3.init()
 voice = engine.getProperty('voices')
 engine.setProperty('voice', voice[0].id)
+rate = engine.getProperty('rate')
+engine.setProperty('rate', 175)
 
 
 def speak(text):
@@ -10,41 +13,75 @@ def speak(text):
     engine.runAndWait()
 
 
+wb = xl.load_workbook('../Datasets/Pokemons Dataset.xlsx')
+sheet = wb.active
+max_rows = sheet.max_row
+max_columns = sheet.max_column
+
+result = "There is no data about this pokemon"
+
+
 def get_pokemon_data(command):
-    import openpyxl as xl
-
-    wb = xl.load_workbook('Pokemons Dataset.xlsx')
-    sheet = wb.active
-    max_rows = sheet.max_row
-    max_columns = sheet.max_column
-
+    global result
     output = []
-    command = command.capitalize()
+    for i in range(1, max_rows + 1):
+        if command == sheet.cell(i + 1, 1).value:
+            index = i + 1
+            for item in range(0, max_columns):
+                output.append(sheet.cell(index, item + 1).value)
 
-    def pokemon_list():
-        pokemon_list = []
-        for i in range(1, max_rows):
-            pokemon_list.append(sheet.cell(i + 1, 1).value)
-        return pokemon_list
+            if output[2] is None:
+                if output[10] == "Nothing":
+                    result = f"{output[0]} is {output[1]} type pokemon," \
+                             f"\nit's attack power is {output[3]}," \
+                             f"\ndefence power is {output[4]}," \
+                             f"\nHP is {output[5]}," \
+                             f"\nspecial attack power is {output[6]}," \
+                             f"\nspecial defence power is {output[7]}," \
+                             f"\nspeed is {output[8]}," \
+                             f"\nit's total power is {output[9]}," \
+                             f"\n{output[0]} is strong against {output[10]}" \
+                             f"\nit is weak against {output[11]} type pokemons."
+                else:
+                    result = f"{output[0]} is {output[1]} type pokemon," \
+                             f"\nit's attack power is {output[3]}," \
+                             f"\ndefence power is {output[4]}," \
+                             f"\nHP is {output[5]}," \
+                             f"\nspecial attack power is {output[6]}," \
+                             f"\nspecial defence power is {output[7]}," \
+                             f"\nspeed is {output[8]}," \
+                             f"\nit's total power is {output[9]}," \
+                             f"\n{output[0]} is strong against {output[10]} type pokemons." \
+                             f"\nit is weak against {output[11]} type pokemons."
+            else:
+                if output[10] == "Nothing":
+                    result = f"{output[0]} is {output[1]} and {output[2]} type pokemon," \
+                             f"\nit's attack power is {output[3]}," \
+                             f"\ndefence power is {output[4]}," \
+                             f"\nHP is {output[5]}," \
+                             f"\nspecial attack power is {output[6]}," \
+                             f"\nspecial defence power is {output[7]}," \
+                             f"\nspeed is {output[8]}," \
+                             f"\nit's total power is {output[9]}," \
+                             f"\n{output[0]} is strong against {output[10]}." \
+                             f"\nit is weak against {output[11]} type pokemons."
+                else:
+                    result = f"{output[0]} is {output[1]} and {output[2]} type pokemon," \
+                             f"\nit's attack power is {output[3]}," \
+                             f"\ndefence power is {output[4]}," \
+                             f"\nHP is {output[5]}," \
+                             f"\nspecial attack power is {output[6]}," \
+                             f"\nspecial defence power is {output[7]}," \
+                             f"\nspeed is {output[8]}," \
+                             f"\nit's total power is {output[9]}," \
+                             f"\n{output[0]} is strong against {output[10]} type pokemons." \
+                             f"\nit is weak against {output[11]} type pokemons."
 
-    if command in pokemon_list():
-        index = pokemon_list().index(command) + 2
-
-        for item in range(1, max_columns + 1):
-            output.append(sheet.cell(index, item).value)
-
-        pokemon, primary_type, secondary_type, attack, defence, HP, sp_attack, sp_defence, speed, total, strong_against, weak_against = \
-        output[0], output[1], output[2], output[3], output[4], output[5], output[6], output[7], output[8], output[9], \
-        output[10], output[11]
-        if secondary_type is None:
-            result = f"{pokemon} is {primary_type} type pokemon,\nit's attack power is {attack},\ndefence power is {defence},\nHP is {HP},\nspecial attack power is {sp_attack},\nspecial defence power is {sp_defence},\nspeed is {speed},\nit's total power is {total},\n{pokemon} is strong against {strong_against} type pokemons.\nit is weak against {weak_against} type pokemons."
-        else:
-            result = f"{pokemon} is {primary_type} and {secondary_type} type pokemon,\nit's attack power is {attack},\ndefence power is {defence},\nHP is {HP},\nspecial attack power is {sp_attack},\nspecial defence power is {sp_defence},\nspeed is {speed},\nit's total power is {total},\n{pokemon} is strong against {strong_against} type pokemons.\nit is weak against {weak_against} type pokemons."
-    else:
-        result = "There is no data about this Pokemon!"
     return result
 
 
-pokemon = get_pokemon_data(input("Enter a Pokemon's Name: "))
-print(pokemon)
-speak(pokemon)
+pokemon = input("Enter any Pokemon: ")
+pokemon = pokemon.title()
+result = get_pokemon_data(pokemon)
+print(result)
+speak(result)
