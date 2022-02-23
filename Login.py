e0ef0d8b931903f8fb7f4cfe05dab3244a35f4f4
Joseph2403 <100103220+Joseph2403@users.noreply.github.com>
2022-02-23 22:52:49 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,6 +1,6 @@
 import openpyxl as xl
 
-workbook = xl.load_workbook("Students List.xlsx")
+workbook = xl.load_workbook("../Datasets/Students List.xlsx")
 sheet = workbook.active
 max_row = sheet.max_row
 max_column = sheet.max_column
@@ -11,14 +11,14 @@ iterate = True
 def details(register_no):
     global iterate, output
     for i in range(2, max_row + 2, 1):
-        if register_no == sheet.cell(i, 3).value:
-            password = input("Password: ")
+        if register_no == str(sheet.cell(i, 3).value):
+            password = input("Password:")
             values = []
             if password == sheet.cell(i, 4).value:
                 for j in range(2, max_column + 1, 1):
                     values.append(sheet.cell(i, j).value)
                     iterate = False
-                output = f"Name: {values[0]}" \
+                output = f"\nName: {values[0]}" \
                          f"\nAge: {values[3]}" \
                          f"\nD.O.B: {values[4]}:" \
                          f"\nBlood Group: {values[5]}" \
@@ -28,12 +28,13 @@ def details(register_no):
                          f"\nQuota: {values[9]}"
                 break
             else:
-                output = "Incorrect Password!\n"
+                output = "Incorrect Password!\nTry Again.\n"
                 break
         else:
-            output = "Invalid Register Number\n"
+            output = "Invalid Register Number!\nTry Again.\n"
     return output
 
-while iterate == True:
-    result = details(int(input("Register Number: ")))
+
+while iterate:
+    result = details(input("Register Number:"))
     print(result)
